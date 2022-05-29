88d13ece6a189269ca5f22e9eb87cf321c176c32
Esteban Mares <97928534+emares17@users.noreply.github.com>
2022-05-29 16:47:49 -0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,72 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+const connectionString = 'mongodb+srv://emares17:Bng313428@star-wars-cluster.x8bctbo.mongodb.net/?retryWrites=true&w=majority'
+
+
+MongoClient.connect(connectionString, { useUnifiedTopology: true })
+  .then(client => {
+    console.log('Connected to Database');
+    const db = client.db('star-wars-quotes');
+    const quotesCollection = db.collection('quotes');
+
+    app.set('view engine', 'ejs')
+    app.use(bodyParser.urlencoded({ extended: true }))
+    app.use(bodyParser.json())
+    app.use(express.static('public'))
+
+    app.listen(3000, function() {
+        console.log('Listening on 3000')
+    });
+
+    app.get('/', (req, res) => {
+        db.collection('quotes').find().toArray()
+          .then(results => {
+            res.render('index.ejs', { quotes: results })
+          })
+          .catch(error => console.error(error))
+    });
+
+    app.post('/quotes', (req, res) => {
+        quotesCollection.insertOne(req.body)
+          .then(result => {
+            res.redirect('/')
+          })
+    });
+    
+    app.put('/quotes', (req, res) => {
+      quotesCollection.findOneAndUpdate(
+        { name: 'Yoda' },
+        {
+          $set: {
+            name: req.body.name,
+            quote: req.body.quote
+          }
+        },
+        {
+          upsert: true
+        }
+      )
+        .then(result => {
+          res.json('success')
+        })
+        .catch(error => console.error(error))
+    })
+    
+    app.delete('/quotes', (req, res) => {
+      quotesCollection.deleteOne(
+        { name: req.body.name },
+      )
+        .then(result => {
+          if (result.deletedCount === 0) {
+            return res.json('No quote to delete')
+          }
+          res.json(`Deleted Darth Vadar's quote`)
+        })
+        .catch(error => console.error(error))
+    })
+  });
+
+
+//   CRUD-CREATE (CONTINUED)
